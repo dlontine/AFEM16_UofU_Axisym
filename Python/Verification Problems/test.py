e3999da2d6333c0b8ec6bf5b9b77665ccdc657a4
dlontine <dereklontine@gmail.com>
2016-04-18 21:15:37 -0600

    Test parallel path commit
    Simple changes to test.py

--- Python/Verification Problems/test.py
+++ Python/Verification Problems/test.py
@@ -8,14 +8,21 @@ import subprocess
 def test_1():
     #This test verifies that the center displacement in a disc that is pressure loaded on one side and simply supported on the outside edge of the other.
 	# See schematic in documentation (XXXX) for more information.
+	E = 1e6
+	v = 0.3
 	
 	#Construct the FEM solution
 	#Mesh
+	#Loading Conditions
+	#Evaluation of key solution point
 	#
+	
 	#Construct the exact solution
 	#Compare the solutions (export solution comparison as png)
 	#Assert a tolerable error to pass/fail test
-    E = 1e6
+
+	
+	
     assert np.allclose(E/1e6, 1, atol=1e-3)
     
 def test_2():
@@ -27,5 +34,6 @@ def test_2():
 	#Compare the solutions (export solution comparison as png)
 	#Assert a tolerable error to pass/fail test
     E = 1e6
+	v = 0.3
     assert np.allclose(E/1e6, 1, atol=1e-3)
     
