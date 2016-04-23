c19c0a3da494ef5da39a252a3be68999023bb416
dlontine <dereklontine@gmail.com>
2016-04-23 17:04:45 -0600

    Update to FEM Test methods

--- Python/Verification Problems/nSolutions/Definitions.py
+++ Python/Verification Problems/nSolutions/Definitions.py
@@ -8,226 +8,256 @@ import sys
 sys.path.insert(0, '../')
 from pyfem2 import *
 
-def Plate_Point_Clamped(E,v,P,eletyp=None):
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    
-    kp1=nej*(nei+1)+1 #Central point
-    kp2=nei+1         #Bottom outside edge
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4)
-    mat = Material('Material-1', elastic={'E':1e6, 'Nu':0.2})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.FixNodes(IHI)
-    step = V.StaticStep()
-    step.ConcentratedLoad(kp1, Y, -100)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-
-Plate_Point_Clamped()
-
-def Plate_Point_Pinned():
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    
-    kp1=nej*(nei+1)+1 #Central point
-    kp2=nei+1         #Bottom outside edge
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4)
-    mat = Material('Material-1', elastic={'E':1e6, 'Nu':0.2})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.PinNodes(kp2)
-    step = V.StaticStep()
-    step.ConcentratedLoad(kp1, Y, -100)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-
-Plate_Point_Pinned()
-
-def Plate_Pressure_Pinned():
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    
-    kp1=nei+1         #Bottom outside edge
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4)
-    mat = Material('Material-1', elastic={'E':1e6, 'Nu':0.2})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.PinNodes(kp1)
-    step = V.StaticStep()
-    step.Pressure(JHI, 100)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-Plate_Pressure_Pinned()
-
-def Plate_Pressure_Clamped():
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4)
-    mat = Material('Material-1', elastic={'E':1e6, 'Nu':0.2})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.FixNodes(IHI)
-    step = V.StaticStep()
-    step.Pressure(JHI, 100)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-Plate_Pressure_Clamped()
-
-def Washer_Point_Pinned(E,v):
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    #Define key points for use in applying loads
-    kp1=nej*(nei+1)+1 #Central point
-    kp2=nei+1         #Bottom outside edge
-    
-    insided=5    
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4,shift=[insided,0])
-    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.PinNodes(kp2)
-    step = V.StaticStep()
-    step.ConcentratedLoad(kp1, Y, -1000)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-Washer_Point_Pinned(1e6,.1)
-
-def Washer_Point_Clamped(E,v):
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    #Define key points for use in applying loads
-    kp1=nej*(nei+1)+1 #Central point
-    kp2=nei+1         #Bottom outside edge
-    
-    insided=5    
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4,shift=[insided,0])
-    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.FixNodes(IHI)
-    step = V.StaticStep()
-    step.ConcentratedLoad(kp1, Y, -1000)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-Washer_Point_Clamped(1e6,.1)
-
-def Washer_Pressure_Pinned(E,v):
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    #Define key points for use in applying loads
-    kp1=nej*(nei+1)+1 #Central point
-    kp2=nei+1         #Bottom outside edge
-    
-    insided=5    
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4,shift=[insided,0])
-    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
-    
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
-    V.ElementBlock('ElementBlock1', ALL)
-    V.AssignProperties('ElementBlock1', AxiSymmetricQuad4, mat)
-    
-    step = V.StaticStep()
-    step.PinNodes(kp2)
-    step = V.StaticStep()
-    step.Pressure(JHI, 100)
-    step.run()
-    V.WriteResults()
-    if not os.environ.get('NOGRAPHICS'):
-        V.Plot2D(show=1, deformed=1)
-Washer_Pressure_Pinned(1e6,.1)
-
-def Washer_Pressure_Clamped(E,v,eletyp=None):
+def Plate_Point_Pinned(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None):
     if eletyp is None:
         eletyp = AxiSymmetricQuad4
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    #Define key points for use in applying loads
+    if NinX is None:
+        nei = 41 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    R=OD/2.0
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=R, ly=h)
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='PlatePointPinned')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.PinNodes(kp2)
+    step = V.StaticStep()
+    step.ConcentratedLoad(kp1, Y, -P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+def Plate_Point_Clamped(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei=41 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej=4 #Number elements in J (Thickness)
+    R=OD/2.0
+    #Find Keypoints of interest
     kp1=nej*(nei+1)+1 #Central point
     kp2=nei+1         #Bottom outside edge
     
-    insided=5    
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4,shift=[insided,0])
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=R, ly=h)
     mat = Material('Material-1', elastic={'E':E, 'Nu':v})
     
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
+    V = FiniteElementModel(mesh=mesh, jobid='PlatePointClamped')
     V.ElementBlock('ElementBlock1', ALL)
     V.AssignProperties('ElementBlock1', eletyp, mat)
     
     step = V.StaticStep()
     step.FixNodes(IHI)
     step = V.StaticStep()
-    step.Pressure(JHI, 100)
+    step.ConcentratedLoad(kp1, Y, -P)
     step.run()
     V.WriteResults()
     if not os.environ.get('NOGRAPHICS'):
         V.Plot2D(show=1, deformed=1)
-Washer_Pressure_Clamped(1e6,.1,AxiSymmetricQuad4Reduced)
-Washer_Pressure_Clamped(1e6,.1,AxiSymmetricQuad4)
-Washer_Pressure_Clamped(1e6,.1,AxiSymmetricQuad4SelectiveReduced)
 
-
-def Thick_Infinite_Cyl(E,v,P,eletyp=None):
+def Plate_Pressure_Pinned(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None):
     if eletyp is None:
         eletyp = AxiSymmetricQuad4
-    nej=5   #Number elements in J (Thickness)
-    nei=21  #Number of elements in I (Diameter)
-    #Define key points for use in applying loads
+    if NinX is None:
+        nei = 41 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    R=OD/2.0
     kp1=nej*(nei+1)+1 #Central point
     kp2=nei+1         #Bottom outside edge
-    
-    insided=5    
-    
-    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=11.5, ly=0.4,shift=[insided,0])
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=R, ly=h)
     mat = Material('Material-1', elastic={'E':E, 'Nu':v})
     
-    V = FiniteElementModel(mesh=mesh, jobid='QuadPlate')
+    V = FiniteElementModel(mesh=mesh, jobid='PlatePressurePinned')
     V.ElementBlock('ElementBlock1', ALL)
     V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.PinNodes(kp2)
+    step = V.StaticStep()
+    step.Pressure(JHI, P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+
+def Plate_Pressure_Clamped(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 41 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    R=OD/2.0
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=R, ly=h)
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='PlatePointPinned')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.FixNodes(IHI)
+    step = V.StaticStep()
+    step.Pressure(JHI, P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+def Washer_Point_Pinned(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None,InsideD=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 20 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    if InsideD is None:
+        InsideD = OD/2.0 
+    R  = OD/2.0
+    Ri = InsideD/2.0
+    w  = R-Ri
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=w, ly=h,shift=[Ri,0])
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='WasherPointPinned')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.PinNodes(kp2)
+    step = V.StaticStep()
+    step.ConcentratedLoad(kp1, Y, -P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+def Washer_Point_Clamped(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None,InsideD=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 20 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    if InsideD is None:
+        InsideD = OD/2.0 
+    R  = OD/2.0
+    Ri = InsideD/2.0
+    w  = R-Ri
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=w, ly=h,shift=[Ri,0])
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='WasherPointClamped')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.FixNodes(IHI)
+    step = V.StaticStep()
+    step.ConcentratedLoad(kp1, Y, -P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+def Washer_Pressure_Pinned(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None,InsideD=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 20 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    if InsideD is None:
+        InsideD = OD/2.0 
+    R  = OD/2.0
+    Ri = InsideD/2.0
+    w  = R-Ri
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=w, ly=h,shift=[Ri,0])
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='WasherPressurePinned')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.PinNodes(kp2)
+    step = V.StaticStep()
+    step.Pressure(JHI, P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+def Washer_Pressure_Clamped(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None,InsideD=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 20 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 4 #Number elements in J (Thickness)
+    if InsideD is None:
+        InsideD = OD/2.0 
+    R  = OD/2.0
+    Ri = InsideD/2.0
+    w  = R-Ri
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=w, ly=h,shift=[Ri,0])
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='WasherPressurePinned')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    step = V.StaticStep()
+    step.FixNodes(IHI)
+    step = V.StaticStep()
+    step.Pressure(JHI, P)
+    step.run()
+    V.WriteResults()
+    if not os.environ.get('NOGRAPHICS'):
+        V.Plot2D(show=1, deformed=1)
+
+
+def Thick_Infinite_Cyl(E,v,P,OD,h,NinX=None,NinY=None,eletyp=None,InsideD=None):
+    if eletyp is None:
+        eletyp = AxiSymmetricQuad4
+    if NinX is None:
+        nei = 20 #Number of elements in I (Diameter)
+    if NinY is None:
+        nej = 10 #Number elements in J (Thickness)
+    if InsideD is None:
+        InsideD = OD/2.0 
+    R  = OD/2.0
+    Ri = InsideD/2.0
+    w  = R-Ri
+    kp1=nej*(nei+1)+1 #Central point
+    kp2=nei+1         #Bottom outside edge
+    mesh = RectilinearMesh2D(nx=nei, ny=nej, lx=w, ly=h,shift=[Ri,0])
+    mat = Material('Material-1', elastic={'E':E, 'Nu':v})
+    
+    V = FiniteElementModel(mesh=mesh, jobid='InfiniteCylinder')
+    V.ElementBlock('ElementBlock1', ALL)
+    V.AssignProperties('ElementBlock1', eletyp, mat)
+    
+    
     V.PrescribedBC(JLO,Y)
     V.PrescribedBC(JHI,Y)
     
@@ -239,4 +269,15 @@ def Thick_Infinite_Cyl(E,v,P,eletyp=None):
     V.WriteResults()
     if not os.environ.get('NOGRAPHICS'):
         V.Plot2D(show=1, deformed=1)
-Thick_Infinite_Cyl(1e4,.3,100)
+
+
+#E,v,P,OD,h,NinX=None,NinY=None,eletyp=None
+Plate_Point_Pinned(1e6,.2,100,23,.4)
+Plate_Point_Clamped(1e6,.2,100,23,.4)
+Plate_Pressure_Pinned(1e6,.2,100,23,.4)
+Plate_Pressure_Clamped(1e6,.2,100,23,.4)
+Washer_Point_Pinned(1e6,.2,100,23,.4)
+Washer_Point_Clamped(1e6,.2,100,23,.4)
+Washer_Pressure_Pinned(1e6,.2,100,23,.4)
+Washer_Pressure_Clamped(1e6,.2,100,23,.4)
+Thick_Infinite_Cyl(1e6,.2,100,23,10)
